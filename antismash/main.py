57c4175a4007db90f54c341911b52b7bd5640540
Caner Bağcı <caner.bagci@uni-tuebingen.de>
2026-09-24 16:58:39 +0200

    Bump version to 0.1.5

--- antismash/main.py
+++ antismash/main.py
@@ -53,7 +53,7 @@ from antismash.outputs import html
 from antismash.support import genefinding
 from antismash.custom_typing import AntismashModule
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 UPSTREAM_VERSION = "8.0.4"  # antiSMASH version this fork is synced with; bumped by sync-upstream.yml
 
 
